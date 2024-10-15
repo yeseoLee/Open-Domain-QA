93787f87b8ec31e5997586d917cb13b9d7139631
Sujinkim-625 <hidong1015@dgu.ac.kr>
2024-10-15 09:59:39 +0900

    feature: 필요없는 주석 삭제

--- train_for_training/arrow_to_csv.py
+++ train_for_training/arrow_to_csv.py
@@ -1,13 +1,9 @@
-import pandas as pd
 from datasets import Dataset
 
-# Load the dataset from the arrow file
 ds = Dataset.from_file("input_file_name.arrow")
 
-# Convert the Dataset object to a pandas DataFrame
 df = ds.to_pandas()
 
-# Save the DataFrame to a CSV file
 csv_file_path = "output_file_name.csv"
 df.to_csv(csv_file_path, index=False)
 
