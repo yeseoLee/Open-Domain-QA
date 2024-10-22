cbbe1493c920a5539bce4b25174ef3347bc3710d
hsmin9809 <hsmin4778@naver.com>
2024-10-22 12:14:07 +0000

    feat: dense train 파일 수정

--- code/dense_encoder_train.py
+++ code/dense_encoder_train.py
@@ -16,8 +16,36 @@ from transformers import (
     AutoTokenizer,
     TrainingArguments,
     AdamW, get_linear_schedule_with_warmup,
+    BertModel, BertPreTrainedModel
 )
-from BertEncoder import BertEncoder
+
+
+class BertEncoder(BertPreTrainedModel):
+
+    def __init__(self,
+        config
+    ):
+        super(BertEncoder, self).__init__(config)
+
+        self.bert = BertModel(config)
+        self.init_weights()
+
+
+    def forward(self,
+            input_ids,
+            attention_mask=None,
+            token_type_ids=None
+        ):
+
+        outputs = self.bert(
+            input_ids,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids
+        )
+
+        pooled_output = outputs[1]
+        return pooled_output
+
 
 class DenseRetrieval:
 
