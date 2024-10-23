ab70134414b1ce6708277ab4bbf11e3134dd290a
yeseoLee <yscoder3893@gmail.com>
2024-10-23 14:03:35 +0900

    fix: 오탈자 수정 및 작은 옵션 변경 - integration_pipeline에 retriever_file명 수정하여 train->test로 자동 전환 옵션 추가 - 오탈자 수정

--- code/retrieval/reranker.py
+++ code/retrieval/reranker.py
@@ -1,3 +1,13 @@
+if __name__ == "__main__":
+    import sys, os
+
+    sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+    import sys
+    import csv
+
+    csv.field_size_limit(sys.maxsize)
+
+
 import ast
 import numpy as np
 import pandas as pd
